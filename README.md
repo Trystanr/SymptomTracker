54f8e3ea68bc6d56cef785e57fb190393a8f3cc4
Trystan Rivers Desktop <trystan.b@hotmail.com>
2020-05-06 09:23:50 +0200

    Implemented Symptom Data Model Added Db Context to startup Added Symptom Controller
    Updated Database

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # SymptomTracker
- IDV300 Term 2
+##IDV300 Term 2
+
+### Install Prerequisites
+`Install-Package Microsoft.EntityFrameworkCore.SqlServer`
+`Install-Package Microsoft.EntityFrameworkCore.Tools`
+
+### Migrate the Database
+`Add-Migration InitialCreate`
+`Update-Database`
---
+++ SymptomTracker/Data/MvcSymptomContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+using SymptomTracker.Models;
+
+namespace SymptomTracker.Data
+{
+    public class MvcSymptomContext : DbContext
+    {
+
+        public MvcSymptomContext (DbContextOptions<MvcSymptomContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Symptom> Symptom { get; set; }
+
+    }
+}
